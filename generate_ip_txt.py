308707c413b44de933252a67679272fa5ed82bb5
cutepan <cutepan@gmail.com>
2025-06-27 14:56:45 +0800

    添加浏览器的ua访问

--- generate_ip_txt.py
+++ generate_ip_txt.py
@@ -10,7 +10,7 @@ if not SUBSCRIBE_URL:
     exit(1)
 
 headers = {
-    "User-Agent": "clash"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
 }
 
 def parse_vmess(line):
